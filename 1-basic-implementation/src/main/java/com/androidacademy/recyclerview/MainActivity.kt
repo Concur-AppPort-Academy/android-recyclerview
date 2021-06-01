9cebf51501e26b2a5bb1ec7a4c3564a10eb9fb95
Oguz <ouzemre@gmail.com>
2021-06-01 14:18:18 +0200

    RecyclerView Course 2

--- 1-basic-implementation/src/main/java/com/androidacademy/recyclerview/MainActivity.kt
+++ 1-basic-implementation/src/main/java/com/androidacademy/recyclerview/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         setRecyclerView()
     }
 
-    private fun generateRandomStrings(length: Int = 1, repeat: Int = 10) : List<String> {
+    private fun generateRandomStrings(length: Int = 1, repeat: Int = 10) : MutableList<String> {
         val strings: MutableList<String> = mutableListOf()
         val allowedChars = ('A'..'Z')
 
@@ -37,26 +39,49 @@ class MainActivity : AppCompatActivity() {
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = ItemAdapter(generateRandomStrings())
+            adapter = ItemAdapter(generateRandomStrings()){ position->
+                onItemClicked(position)
+            }
         }
+
+        val button = findViewById<Button>(R.id.addButton)
+        button.setOnClickListener{
+            val adapter = recyclerView.adapter as ItemAdapter
+            adapter.addItem("Random")
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun onItemClicked(item: Int){
+        Toast.makeText(this, "Item Clicked $item", Toast.LENGTH_SHORT).show()
     }
 }
 
-class ItemAdapter(private val dataSet: List<String>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+class ItemAdapter(private val dataSet: MutableList<String>, private val onItemClicked: (Int) -> Unit): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val view =
             LayoutInflater
                 .from(parent.context)
                 .inflate(R.layout.line_item, parent, false)
-        return ItemViewHolder(view)
+        return ItemViewHolder(view, onItemClicked = onItemClicked)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) = holder.bind(position)
     override fun getItemCount(): Int = dataSet.size
 
-    inner class ItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    fun addItem(value: String, position: Int = 0){
+        dataSet.add(position, value)
+    }
+
+    inner class ItemViewHolder(view: View, val onItemClicked: (Int) -> Unit): RecyclerView.ViewHolder(view) {
         private val textItem: TextView = view.findViewById(R.id.text_item)
+
+        init {
+            view.setOnClickListener {
+                onItemClicked(adapterPosition)
+            }
+        }
 
         fun bind(position: Int) {
             textItem.text = dataSet[position]
